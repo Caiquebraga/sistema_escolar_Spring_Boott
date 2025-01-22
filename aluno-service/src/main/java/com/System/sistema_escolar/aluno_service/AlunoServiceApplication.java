d8bf6e43fa420faf258bcbc05cb26bd32a489dd3
Caique.Braga <caiquebsousa@outlook.com>
2025-01-21 21:06:59 -0300

    Initial commit

---
+++ aluno-service/src/main/java/com/System/sistema_escolar/aluno_service/AlunoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.System.sistema_escolar.aluno_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoServiceApplication.class, args);
+	}
+
+}
---
+++ aluno-service/src/test/java/com/System/sistema_escolar/aluno_service/AlunoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.System.sistema_escolar.aluno_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
